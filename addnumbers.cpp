98d5377cbb4641873cc802823d79be83c7e7a2f8
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2022-03-08 12:41:57 -0800

    avg grades script by me

---
+++ addnumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int num1;
+    int num2;
+
+    std::cout << "Enter number 1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter number 2: ";
+    std::cin >> num2;
+
+    std::cout << "Sum = " << num1 + num2;
+    return 0;
+}
---
+++ avggrades.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, num3, num4, num5, num6, sum;
+
+    std::cout << "Enter your 6 grades: ";
+    std::cin >> num1 >> num2 >> num3 >> num4 >> num5 >> num6;
+
+    sum = num1 + num2 + num3 + num4 + num5 + num6;
+
+    std::cout << "Your final grade is: " << sum / 6 << std::endl;
+}
